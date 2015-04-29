6c97866f925fa06cb10091438973e8effee44a32
hubertm <m_hubert@gmx.at>
2015-04-29 16:43:47 +0200

    laste version

--- FSEC/prepare_csv.sh
+++ FSEC/prepare_csv.sh
@@ -8,7 +8,8 @@ then
 else
 echo "Processing $1 ...."
 fi
-
+pdf=${2:-TRUE} #checks if the second argument is present if not sets the variable to TRUE
+echo "Printing pdf: "$pdf
 a=1
 time=0
 intensity=0
@@ -22,7 +23,8 @@ while [ $a -le $runnumber ]
 do
    time=$((8*${a}-1))
    intensity=$((8*${a}))
-   string+=$time","$intensity","
+   #string+=$time","$intensity","
+   string=$string$time","$intensity","
    a=`expr $a + 1`
 done
 #Removing all other colums
@@ -39,6 +41,6 @@ sed '2d' cleaned.csv>Ready.csv
 rm cleaned.csv
 rm trimmed.csv
 #Calling the R scripts which expects a file called Ready.csv as input
-echo "Calling the R script"
-Rscript SEC_curve_area.R
-#rm Ready.csv
+echo "Calling the R script\n"
+Rscript SEC_curve_area_working.R $pdf
+rm Ready.csv
